10069b5c929bee0f3f3137951c8e22967dab4b89
Pablo Lires <pablolires@gmail.com>
2022-12-29 11:33:56 -0300

    desacoplar modulo de contactos generales para reutilizar en otras pages

---
+++ includes/modulo-contacto.php
@@ -0,0 +1,28 @@
+<section class="container-fluid modulo_contacto">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12">
+				<!-- Mensages -->
+				<?php include_once('./includes/msg.php'); ?>
+
+				<!-- Errors -->
+				<?php include_once('./includes/errors.php'); ?>
+			</div>
+
+			<div data-aos="fade-up" class="col-md-9 formulario">
+				<div class="content_formulario">
+					<h3>¿Querés obtener más <br>información de <span>EWIG</span> o <br>tenés alguna pregunta?</h3>
+					<!-- Formulario -->
+					<?php include_once('./includes/parts/formulario.php'); ?>
+				</div>
+			</div>
+
+			<div class="col-md-9">
+				<div class="content_formulario">
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
--- index.php
+++ index.php
@@ -143,38 +143,10 @@
 		<?php include_once('./includes/modulo-arquitectos-distribuidores.php'); ?>
 
 		<!-- Propiedades -->
-		<?php include_once('./includes/modulo-propiedades-producto.php'); ?>		
+		<?php include_once('./includes/modulo-propiedades-producto.php'); ?>
 
 		<!-- Módulo Contacto -->
-		<section class="container-fluid modulo_contacto">
-			<div class="container">
-				<div class="row">
-
-					<div class="col-md-12">
-						<!-- Mensages -->
-						<?php include_once('./includes/msg.php'); ?>
-
-						<!-- Errors -->
-						<?php include_once('./includes/errors.php'); ?>
-					</div>
-
-					<div data-aos="fade-up" class="col-md-9 formulario">
-						<div class="content_formulario">
-							<h3>¿Querés obtener más <br>información de <span>EWIG</span> o <br>tenés alguna pregunta?</h3>
-							<!-- Formulario -->
-							<?php include_once('./includes/parts/formulario.php'); ?>
-						</div>
-					</div>
-
-					<div class="col-md-9">
-						<div class="content_formulario">
-						</div>
-					</div>
-
-				</div>
-			</div>
-		</section>
-		<!-- Módulo Contacto end -->
+		<?php include_once('./includes/modulo-contacto.php'); ?>		
 
 		<!-- Galería Productos -->
 		<section class="container-fluid galeria_productos">
