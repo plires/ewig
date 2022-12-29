e4e569da9f14a0d760e942709b7821a908401097
Pablo Lires <pablolires@gmail.com>
2022-12-29 10:42:43 -0300

    desacoplar modulo de arq y dist para reutilizar en otras pages

--- index.php
+++ index.php
@@ -140,27 +140,7 @@
 		<?php include_once('./includes/modulo-nuestros-sistemas.php'); ?>
 
 		<!-- Arquitectos & Distribuidores -->
-		<section class="container-fluid arq_dist">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 content">
-						<h3 data-aos="fade-up">Arquitectos</h3>
-						<p data-aos="fade-up">
-							Conocé las ventajas de utilizar un producto de vanguardia fabricado bajo los más altos estándares de calidad.
-						</p>
-						<a href="#" data-aos="fade-up" class="btn btn-primary">ver más</a>
-					</div>
-					<div class="col-md-6 content">
-						<h3 data-aos="fade-up">Distribuidores</h3>
-						<p data-aos="fade-up">
-							Formá parte de nuestro gran equipo y sumá a tu oferta la calidad y confiabilidad de un producto líder.
-						</p>
-						<a href="#" data-aos="fade-up" class="btn btn-primary">ver más</a>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- Arquitectos & Distribuidores end -->
+		<?php include_once('./includes/modulo-arquitectos-distribuidores.php'); ?>
 
 		<!-- Propiedades -->
 		<section class="container-fluid propiedades">
