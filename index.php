dab6ae658e1b3515034798ec6d6df64d699eddbc
Pablo Lires <pablolires@gmail.com>
2022-12-29 10:32:04 -0300

    desacoplar modulo de productos para reutilizar en otras pages

--- index.php
+++ index.php
@@ -137,56 +137,7 @@
 		<!-- Frase Descriptiva end -->
 
 		<!-- Nuestros Sistemas -->
-		<section class="container-fluid nuestros_sistemas">
-			<div class="horizontal"><i></i></div>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<h2>Nuestros Sistemas</h2>
-					</div>
-
-					<div class="col-md-12 content_productos">
-						
-						<div data-aos="fade-up" class="producto">
-							<a href="#">
-								<img class="img-fluid transition" src="./img/home/productos/new-elegans.jpg" alt="new elegans">
-							</a>
-							<h3>NEW ELEGANS</h3>
-						</div>
-
-						<div data-aos="fade-up" class="producto">
-							<a href="#">
-								<img class="img-fluid transition" src="./img/home/productos/sliding-7400.jpg" alt="sliding 7400s">
-							</a>
-							<h3>SLIDING 7400</h3>
-						</div>
-
-						<div data-aos="fade-up" class="producto">
-							<a href="#">
-								<img class="img-fluid transition" src="./img/home/productos/compact.jpg" alt="compact">
-							</a>
-							<h3>INOVA</h3>
-						</div>
-
-						<div data-aos="fade-up" class="producto">
-							<a href="#">
-								<img class="img-fluid transition" src="./img/home/productos/hebe-schiebe.jpg" alt="hebe schiebe">
-							</a>
-							<h3>COMPACT</h3>
-						</div>
-
-						<div data-aos="fade-up" class="producto">
-							<a href="#">
-								<img class="img-fluid transition" src="./img/home/productos/inova.jpg" alt="inova">
-							</a>
-							<h3>HEBE SCHIEBE</h3>
-						</div>
-
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- Nuestros Sistemas end -->
+		<?php include_once('./includes/modulo-nuestros-sistemas.php'); ?>
 
 		<!-- Arquitectos & Distribuidores -->
 		<section class="container-fluid arq_dist">
